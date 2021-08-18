8fb2d55c3ab4500fa373e6db199e60b10a2f16c4
s04ar <chakungrao.boy@gmail.com>
2021-08-18 09:57:49 +0700

    Update 18/8
    my class stuff

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { auth } from './firebase'; 
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './Screen/sessions/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [signedIn, setsignedIn] = useState(false);
+  
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setsignedIn(true);
+    } else {
+      setsignedIn(false);
+    }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! uwuowo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={DefaultTheme}>
+      {signedIn
+        ? (
+          <Text>Signed In</Text>
+        ) : (
+          <>
+          <StatusBar style="light" />
+          <Stack.Navigator
+          mode="card"
+          screenOptions={{}}
+          >
+            <Stack.Screen
+            name="signIn"
+            component={LoginScreen}
+            options={{
+              title: 'Sign In',
+              headerStyle: {
+                backgroundColor: '#29434e',
+                borderBottomColor: '#29434e',
+              },
+              headerTintColor: '#fff'
+            }}/>
+            </Stack.Navigator>
+            </>
+        )
+      }
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+)
---
+++ Screen/sessions/LoginScreen.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { TextInput, Button } from 'react-native-paper'
+import styles from './styles';
+const LoginScreen = ({ navigation }) => {
+  
+  const { control, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+  return (
+    <View style={styles.authFormContainer}>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.formInput}
+            label="Email"
+            mode="outlined"
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="email"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      <View style={styles.errorMsg}>
+        {errors.email && <Text style={styles.errorText}>You must fill in your email</Text>}
+      </View>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.formInput}
+            label="Password"
+            mode="outlined"
+            secureTextEntry
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="password"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      <View style={styles.errorMsg}>
+        {errors.password && <Text style={styles.errorText}>You must fill in your password</Text>}
+      </View>
+      <View>
+        <Button
+          style={styles.submitButton}
+          mode="contained"
+          compact={false}
+          onPress={handleSubmit(onSubmit)}
+          icon="account-arrow-right"
+        >
+          Sign in
+        </Button>
+
+      </View>
+
+      <View style={styles.switchScreenText}>
+        <Text>
+          Don't have an account yet?
+        </Text>
+      </View>  
+      <Button
+        mode="outlined"
+        style={styles.switchBtn}
+        icon="account-plus"
+        compact
+        onPress={() => navigation.navigate('register')}>Register Account now beeeeeeeeeich
+      </Button>
+      <p>esreveR Approved woopwoop</p>
+    </View>
+    
+    
+  )
+  const LoginScreen = ({ navigation }) => {
+    
+
+
+  }
+  
+};
+export default LoginScreen;
